257c0148b8749d9fe932bea4ba54393c0d719565
Sameer1697 <samgaonkar2013@gmail.com>
2020-08-22 00:53:52 +0530

    Updates

---
+++ precache-manifest.5c7db4398fa6ff049732c9f845cb166d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd7c12d346c5b5fbf67ba004eb09542",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "1e2465417c84648c2b69",
+    "url": "/netflix-clone/static/css/main.76181f72.chunk.css"
+  },
+  {
+    "revision": "0e1abda21678beb8efda",
+    "url": "/netflix-clone/static/js/2.08cc744c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.08cc744c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2465417c84648c2b69",
+    "url": "/netflix-clone/static/js/main.c3eec11b.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
